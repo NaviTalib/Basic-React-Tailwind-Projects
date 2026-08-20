50b673de38cef329c984af5911e13660e0f86721
NaviTalib <talibali786.tagmail.com>
2026-08-20 13:42:55 +0530

    accordion component done

---
+++ components-collection/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import FaqAccordion from './components/FaqAccordion'
+
+const App = () => {
+  return (
+    <div className="h-screen  flex justify-center items-center">
+        <FaqAccordion />
+    </div>
+  )
+}
+
+export default App
