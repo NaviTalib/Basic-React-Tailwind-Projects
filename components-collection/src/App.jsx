0981dcd64f749d0301a5afc869922c452120a39f
NaviTalib <talibali786.ta@gmail.com>
2026-08-29 14:33:47 +0530

    updetd

--- components-collection/src/App.jsx
+++ components-collection/src/App.jsx
@@ -1,4 +1,4 @@
-import React,{useState} from 'react'
+import React,{useState, useEffect} from 'react'
 import FaqAccordion from './components/FaqAccordion'
 import FlashCardDeck from './components/FlashCardDeck'
 import ProfileCard from './components/ProfileCard'
@@ -15,25 +15,15 @@ const App = () => {
         {/* <ProfileCard name="Navi Talib" role="Full Stack Developer" initialLike={780} /> */}
         {/* <Timer /> */}
         {/* <SearchBox /> */}
-        <ProductCard
+        {/* <ProductCard
           name = "Wireless Headphones"
           image="https://placehold.co/250"
           price={1799}
           rating={5}
-        />
-        {/* <ProductCard
-          name = "Water Bottle"
-          image="https://placehold.co/250"
-          price={99}
-          rating={4.5}
-        />
-        <ProductCard
-          name = "Shoes"
-          image="https://placehold.co/250"
-          price={2399}
-          rating={5}
         /> */}
 
+        <h1>Hello</h1>
+       
 
     </div>
   )
